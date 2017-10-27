00b789174ed8200c97f4fb8eb84030e326ee6d69
npklein <niekdeklein@gmail.com>
2017-10-27 13:19:40 +0200

    change table to html table

--- parsePapers.py
+++ parsePapers.py
@@ -4,6 +4,7 @@ import argparse
 import datetime
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import sys
 
 parser = argparse.ArgumentParser('Get digest of papers channel from last week and, optionally, email it (using gmail for now)\n')
@@ -13,38 +14,46 @@ parser.add_argument("--sender_user", help="gmail username for sender", required=
 parser.add_argument("--sender_password", help="gmail password for sender", required="--emails" in sys.argv)
 args = parser.parse_args()
 
+def html_table(content_list):
+    table  = '<html>\n'
+    table += '  <head></head>\n'
+    table += '  <body>\n'
+    table += '    <table>\n'
+    for sublist in content_list:
+        table += '      <tr><td>'
+        table += '        </td><td>'.join(sublist)
+        table += '      </td></tr>\n'
+    table += '    </table>\n'
+    table += '  </body>\n'
+    table += '</html>'
+    return(table)
 
-
-def send_email(user, pwd, recipient, subject, body):
-    import smtplib
-
-    gmail_user = user
-    gmail_pwd = pwd
-    FROM = user
+def send_email(user, pwd, recipient, subject, plain_text, html):
     TO = recipient if type(recipient) is list else [recipient]
-    SUBJECT = subject
-    TEXT = body
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = user
+    msg['To'] = recipient
+    part1 = MIMEText(plain_text, 'plain')
+    part2 = MIMEText(html, 'html')
 
-    # Prepare actual message
-    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.ehlo()
-        server.starttls()
-        server.login(gmail_user, gmail_pwd)
-        server.sendmail(FROM, TO, message)
-        server.close()
-        print 'successfully sent the mail'
-    except:
-        print "failed to send mail"
+    msg.attach(part1)
+    msg.attach(part2)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(user, pwd)
+    server.sendmail(user, TO,msg.as_string())
+    server.close()
+    print('email sent')
 
 
 papers = json.loads(subprocess.check_output(['slack-history-export','--token',args.token,'--channel','papers']).decode('utf-8'))
 subject = 'Weekly digest Papers channel'
 
-msg = 'Weekly Slack Papers channel links\n'
 
+content_list = []
 for message in papers:
     if 'attachments' in message:
         datetime_now = datetime.date.today()
@@ -54,8 +63,9 @@ for message in papers:
             for attachment in attachments:
                 print(attachment['title'], attachment['title_link'])
                 if args.receiver_emails:
-                    msg += '\n'+attachment['title']+'\t'+attachment['title_link']
+                    content_list.append([attachment['title'],attachment['title_link']])
 
+plain_text_msg = 'Weekly Slack Papers channel links\n\n'
 if args.receiver_emails:
-    for receiver_email in args.receiver_emails.split(','):
-        send_email(args.sender_user, args.sender_password, receiver_email, subject, msg)
+    html_msg = html_table(content_list)
+    send_email(args.sender_user, args.sender_password, args.receiver_emails.split(','), subject, plain_text_msg, html_msg)
