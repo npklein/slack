e379372cb1ec3bc192288c18b86b0e5ccb6a86de
npklein <niekdeklein@gmail.com>
2019-05-08 01:56:01 +0200

    add private channels,groups and private messages

--- download_history/download_history.py
+++ download_history/download_history.py
@@ -1,137 +1,125 @@
-import os
-import logging
 import slack
 import ssl as ssl_lib
 import certifi
 import json
 import subprocess
-from datetime import datetime, timedelta, time
-import requests
+from message import Message
+import os
 
+import argparse
+from werkzeug.datastructures import is_immutable
 
-def send_message(message, channel, web_client):
-    message_block = {
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": (
-                message
-            ),
-        },
-    }
+parser = argparse.ArgumentParser(description='Download all available slack messages up until last midnight.')
+parser.add_argument('token', help='Slack Token. You can generate it from here https://api.slack.com/custom-integrations/legacy-tokens')
+parser.add_argument('outdir', help='Direcotry to write slack message files to')
+parser.add_argument('--exclude_channels', help='Comma separated list of channels to exclude')
 
-    payload = {
-        "channel": channel,
-        'username': 'history_downloader',
-        "icon_emoji": ":robot_face:",
-        "blocks": [
-            message_block,
-        ]}
-    # Post the onboarding message in Slack
-    response = web_client.chat_postMessage(**payload)
+args = parser.parse_args()
 
-def start_downloading(web_client: slack.WebClient):
-    '''Download all channels chat history. Download up to and including yesterday. Then transfer to the cluster'''
-    print('finding the right channel to write messages to')
-    
-    excluded_channels = set(['lunch','random'])
-    
-    for channel in web_client.conversations_list()['channels']:
-        channel_name = web_client.channels_info(channel=channel['id'])['channel']['name']
-        if channel_name == 'download_history':
-            channel_message_id = channel['id']
-    send_message('Start downloading all history',channel_message_id, web_client)
-    try:
+if args.exclude_channels:
+    print('Downloading chat history of channels, excluding '+ args.exclude_channels)
+else:
+    print('Downloading chat history of all channels')
+
+class Downloader():
+    def __init__(self, web_client):
+        self.web_client = web_client
+        
+    def start_downloading_messages(self, outdir):
+        '''Download all channels chat history. Download up to and including yesterday. Then transfer to the cluster'''
+                
         print('start downloading history')
-
-            
-        #send_message('Start downloading messages',web_client.conv 'test')
-        # get date/time at midnight so only messages from yesterday can be downloaded
-        midnight = datetime.combine(datetime.today(), time.min)
+        
         # Get all the channel IDs
-        for channel in web_client.conversations_list()['channels']:
-            
+        for channel in self.web_client.conversations_list(types='public_channel,private_channel,mpim')['channels']:
             # Convert channel ID to channel name
-            channel_name = web_client.channels_info(channel=channel['id'])['channel']['name']
             
+
             # exclude some channels
             
-            if channel_name in excluded_channels:
+
+            # Get the history of the channel (tried to do it using the python API but did not work, so using slack-history-export)
+            if channel['is_im']:
+                search_type = 'username'
+                name = None
+                for user in self.web_client.users_list()['members']:
+                    if user['id'] == channel['user']:
+                        name = user['name']
+                        break
+                if not name:
+                    raise RuntimeError('User ID '+channel['id']+' not found')
+            elif channel['is_channel']:
+                search_type = 'channel'
+                name = channel['name']
+
+            elif channel['is_group']:
+                search_type = 'group'
+                name = channel['name']
+
+            outdir = args.outdir+'/'+name+'/'
+            print('downloading messages from '+name+'. Might take a bit.')
+
+            if args.exclude_channels and name in args.exclude_channels.split(','):
                 continue
             
-            send_message('Downloading '+channel_name,channel_message_id, web_client)
-            # Get the history of the channel (tried to do it using the python API but did not work, so using slack-history-export)
-            subprocess_output = subprocess.check_output(['slack-history-export','--token',web_client.token,'--channel',channel_name])
+            slack_command = ['slack-history-export','--token',
+                                          self.web_client.token, '--'+search_type, name]
+            subprocess_output = subprocess.check_output(slack_command)
             history = json.loads(subprocess_output.decode('utf-8'))
-            
             prev_day = None
             out = None
-            for message in history:
-                message['isoDate'] = message['isoDate'].split('.')[0]
-                message_date = datetime.strptime(message['isoDate'], "%Y-%m-%dT%H:%M:%S")
-                message_date_str = message_date.strftime('%Y-%m-%d')
-                if(message_date >= midnight):
+            slack_message = None
+            for message_data in history:
+                # this is in the for loop instead of out because only want to make it if there is at least 1
+                # output file to be written
+                if not os.path.exists(outdir):
+                    os.makedirs(outdir)
+            
+                slack_message = Message(message_data, name, self.web_client)
+                outfile = name + slack_message.date_str+'.txt'
+                if(slack_message.is_today()):
                     # don't download messages from today
                     continue
                 
                 # download files from messages
-                if 'files' in message:
-                    for file_info in message['files']:
-                        file_name = "".join([x if x.isalnum() else "_" for x in file_info['title']])+'.'+file_info['filetype']
-                        download_link = file_info['url_private_download']
-                        channel_dir = 'slack_history/'+channel_name+'/'+message_date_str
-                        if not os.path.exists(channel_dir):
-                            os.makedirs(channel_dir)
-                        print('downloading '+download_link+' to' +channel_dir+'/'+file_name)
-                        r = requests.get(download_link, headers={'Authorization': 'Bearer %s' % web_client.token})
-                        open( channel_dir+'/'+file_name, 'wb').write(r.content)
-                    
-                # parse the message and organize by date
-                user_info = web_client.users_info(user=message['user'])['user']
-                if 'real_name' in user_info:
-                    user_name = user_info['real_name']
-                elif 'username' in user_info:
-                    user_name = user_info['username']
-                else:
-                    user_name = 'Unknown'
-                message_text = message['text']
-                if message_text.endswith('has joined the channel') or message_text.endswith('has left the channel'):
-                    continue 
-                    
-                if prev_day != message_date_str:
+                if slack_message.has_file:
+                    for file_name, content in slack_message.download_files():
+                        files_dir = 'slack_history/'+name+'/'+slack_message.date_str
+                        if not os.path.exists(files_dir):
+                            os.makedirs(files_dir)
+                        if os.path.exists(files_dir+'/'+file_name):
+                            continue
+                        open( files_dir+'/'+file_name, 'wb').write(content)
+    
+                # because we have multiple messages from different dates, check when the messages are from a different date, then write to different file
+                if slack_message.date_str != prev_day:  
                     if out:
                         out.close()
-                        
-                    channel_dir = 'slack_history/'+channel_name+'/'+message_date_str+'/'
-                    if not os.path.exists(channel_dir):
-                        os.makedirs(channel_dir)
-                    out = open(channel_dir+message_date_str+'.txt','w')
-                    
-                out.write(user_name+'::'+message_date.strftime("%Y-%m-%d-%H:%M:%S")+':: '+message_text+'\n')
-                
-                prev_day = message_date_str
-        send_message('Finished downloading all messages',channel_message_id, web_client)
-    except Exception as e:
-        send_message('Got an error\n'+str(e),channel_message_id, web_client)
-        raise
+                    out = open(outdir+'/'+ slack_message.date_str+'.txt','w')
+                text = slack_message.parse_text()
+                if text:
+                    out.write(text)
+        
+
 # ============== Message Events ============= #
-# When a user sends a DM, the event type will be 'message'.
-# Here we'll link the update_share callback to the 'message' event.
+# When a user sends a DM, the event type will be 'slack_message'.
+# Here we'll link the update_share callback to the 'slack_message' event.    
+
 @slack.RTMClient.run_on(event="message")
-def message(**payload):
-    """Download all messages of current channel when getting a message
-    that contains "download".
+def react_to_message(**payload):
+    """Download all messages of current channel when getting any message (using this as trigger as I haven't figured out how to let it run without trigger)
     Execute after the first message is received, then exit(). Allows for running this only once a day
     """
     web_client = payload["web_client"]
-    start_downloading(web_client)
 
+    downloader = Downloader(web_client)
+    downloader.start_downloading_messages(args.outdir)
+    exit()
  
     
 if __name__ == "__main__":
     ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
-    with open('/Users/NPK/UMCG/git_projects/slack/exportPapers/token.txt') as input_file:
-        slack_token = input_file.read().strip()
+    slack_token = args.token
     rtm_client = slack.RTMClient(token=slack_token, ssl=ssl_context)
     rtm_client.start()
     
---
+++ download_history/message.py
@@ -0,0 +1,69 @@
+from datetime import datetime, time
+import os
+import requests
+
+class Message():
+    '''Save json data of message'''
+    def __init__(self, message_json, channel_name, web_client):
+        self.isoData = message_json['isoDate'].split('.')[0]
+        self.datetime = datetime.strptime(self.isoData, "%Y-%m-%dT%H:%M:%S")
+        self.date_str = self.datetime.strftime('%Y-%m-%d')
+        self.has_file = False
+        self.channel_name = channel_name
+        if 'files' in message_json:
+            self.has_file = True
+            self.files = message_json['files']
+        
+        self.message_text = message_json['text']        
+        self.web_client = web_client
+        
+        if 'user' in message_json:
+            user_info = web_client.users_info(user=message_json['user'])['user']
+        else:
+            self.user_name = 'Unknown'
+            return
+            
+        if 'real_name' in user_info:
+            self.user_name = user_info['real_name']
+        elif 'username' in user_info:
+            self.user_name = user_info['username']
+        else:
+            self.user_name = 'Unknown'
+
+    
+    def is_today(self):
+        '''Return True if message is from today, else False'''
+        # get date/time at midnight so only messages from yesterday can be downloaded
+        midnight = datetime.combine(datetime.today(), time.min)
+        return(self.datetime >= midnight)
+    
+    def download_files(self):
+        '''If the message contains a file, download it'''
+        for file_info in self.files:
+            if 'title' in file_info:
+                file_name = "".join([x if x.isalnum() else "_" for x in file_info['title']])
+            else:
+                file_name = message_date_str+'_noTitle'
+            if 'filetype' in file_info:
+                extension = file_info['filetype']
+            else:
+                extension = 'uunknown'
+            file_name.rstrip('_'+extension)
+            file_name = file_name + '.' + extension
+            if not 'url_private_download' in file_info:
+                continue
+            download_link = file_info['url_private_download']
+            r = requests.get(download_link, headers={'Authorization': 'Bearer %s' % self.web_client.token})
+            
+            yield (file_name, r.content)
+            
+    
+    def parse_text(self):
+        '''Parse the text message and save the info'''
+        # parse the message and organize by date
+        
+        if self.message_text.endswith('has joined the channel') or self.message_text.endswith('has left the channel'):
+            return None
+            
+
+        return(self.user_name+'::'+self.datetime.strftime("%Y-%m-%d-%H:%M:%S")+':: '+self.message_text+'\n')    
